f7fccde9e84b4ad48ece4f998294fa76b06c0274
Kevin Rosario <29806319+KevinRosario01@users.noreply.github.com>
2024-06-03 23:50:45 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let exts = [".com", ".net", ".org"];
+  let domains = [];
+
+  for (const p of pronouns) {
+    for (const a of adjs) {
+      for (const n of nouns) {
+        for (const e of exts) {
+          const d = p + a + n + e;
+          domains.push(d);
+        }
+      }
+    }
+  }
+  let formattedDomains = domains.join("<br>");
+
+  console.log(formattedDomains);
+
+  document.querySelector("#domain").innerHTML = formattedDomains;
 };
